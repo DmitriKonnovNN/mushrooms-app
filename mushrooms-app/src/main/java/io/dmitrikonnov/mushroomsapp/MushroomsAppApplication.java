d41c7dee4c81bad512b7ae267d0e3757304214b2
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2022-01-20 17:32:41 +0300

    Initial commit

---
+++ mushrooms-app/src/main/java/io/dmitrikonnov/mushroomsapp/MushroomsAppApplication.java
@@ -0,0 +1,13 @@
+package io.dmitrikonnov.mushroomsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushroomsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MushroomsAppApplication.class, args);
+	}
+
+}
---
+++ mushrooms-app/src/test/java/io/dmitrikonnov/mushroomsapp/MushroomsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dmitrikonnov.mushroomsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushroomsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
